51108f4dc9450a0e775f0fcee3d7fc9295d653c2
Elabar <leehoemun@gmail.com>
2019-03-09 01:50:14 +0800

    rebuilt IEventList class based on class diagram

--- src/event/domain/IEventList.java
+++ src/event/domain/IEventList.java
@@ -3,12 +3,8 @@ package event.domain;
 import java.util.List;
 
 public interface IEventList {
-	public void addEvent(String title,String venue,
-			String theme,String date);
-	public int getNumberOfEvents();
-	public List<Event> getEvents();
-	public void addTalk(int selectedEvent,double duration,String title,String speaker);
-	public List<Talk> getTalks(int selectedEvent);
-	public void addGuest(int selectedEvent,String name,String contact);
-	public List<Guest> getGuests(int selectedEvent);
+	public Event selectEvent(int index);
+	public int getNumberOfEvent();
+	public List<Event> getAllEvent();
+	public void addEvent(Event anEvent);
 }
