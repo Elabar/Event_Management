4600ed459ff82d3ea26b7861e5a8f3903acf7290
Elabar <leehoemun@gmail.com>
2019-03-09 01:59:34 +0800

    controller is being build and found defects in class diagram,sequence diagram

--- src/event/domain/Controller.java
+++ src/event/domain/Controller.java
@@ -1,5 +1,18 @@
 package event.domain;
 
 public class Controller {
-
+	private IEventList eventList;
+	
+	public void updateGuestStatus(Guest aGuest,String status) {
+		aGuest.setReplyStatus(status);
+	}
+	
+	public Guest selectGuest(int index,Event anEvent) {
+		return anEvent.getAllGuest().get(index);
+	}
+	
+	public Event selectEvent(int index) {
+		return eventList.getAllEvent().get(index);
+	}
+	
 }
